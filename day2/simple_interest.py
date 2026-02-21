083d916590d2f009bdc79c90382d24066beccd89
Komala MN <komala2001mn@gmail.com>
2026-02-22 00:51:22 +0530

    day2/simple_interest.py

---
+++ day2/simple_interest.py
@@ -0,0 +1,6 @@
+principal= float(input("enter the principal amount: "))
+rate= float(input("enter the rate of interest: "))
+time=float(input("enter the time (in year):  "))
+
+interest_rate= (principal* rate* time)/100
+print("simple interest is",interest_rate)
