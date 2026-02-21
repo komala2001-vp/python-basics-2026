022db463096d4eb6be4582470ac997f6f64115db
Komala MN <komala2001mn@gmail.com>
2026-02-22 00:39:48 +0530

    day2/area_rectangle.py

---
+++ day1/day2/area_rectangle.py
@@ -0,0 +1,8 @@
+# Program to calculate area of rectangle
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+area = length * width
+
+print("Area of rectangle is:", area)
