d363683f54b2d58bfe6157907dfcfdf6e6d6ead3
Komala MN <komala2001mn@gmail.com>
2026-02-22 00:00:40 +0530

    day1/square_number.py

---
+++ square_number.py
@@ -0,0 +1,9 @@
+# Program to print square of a number
+
+number = int(input("Enter a number: "))
+
+square_value = number * number
+
+print("Square of the number is:", square_value)
+
+# we can also write square_value = number ** 2
