15c9093771c15800e00ed2d2e580ead202c8f683
Komala MN <komala2001mn@gmail.com>
2026-02-23 00:33:11 +0530

    day4/ grade_calculator.py

---
+++ day4/grade_calculator.py
@@ -0,0 +1,15 @@
+#  Grade Calculator
+
+mark = int(input("Enter your marks: "))
+
+if mark >= 75:
+    print("Distinction")
+
+elif mark >= 60:
+    print("First Class")
+
+elif mark >= 50:
+    print("Second Class")
+
+else:
+    print("Fail")
