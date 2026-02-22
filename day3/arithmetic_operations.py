2e99e5f5c1fbb6095a4daaa3aa8c44a26999e883
Komala MN <komala2001mn@gmail.com>
2026-02-22 22:53:39 +0530

    day3/arithmetic_operations.py

---
+++ day3/arithmetic_operations.py
@@ -0,0 +1,19 @@
+# Taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Performing operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+floor_division = num1 // num2
+remainder = num1 % num2
+
+# Printing results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division result:", division)
+print("Floor division result:", floor_division)
+print("Remainder:", remainder)
