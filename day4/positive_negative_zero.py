c3107460a2a04646a609009d3d7e0f9a305b9d1e
Komala MN <komala2001mn@gmail.com>
2026-02-22 23:07:21 +0530

    day4/positive_negative_zero.py

---
+++ day4/positive_negative_zero.py
@@ -0,0 +1,15 @@
+#  Check if a number is Positive, Negative or Zero
+# This program uses if-elif-else condition
+
+# Taking input from user
+number = int(input("Enter a number: "))
+
+# Checking the number
+if number > 0:
+    print("The number is Positive")
+
+elif number < 0:
+    print("The number is Negative")
+
+else:
+    print("The number is Zero")
