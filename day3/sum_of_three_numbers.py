33ec1f240b3f0418f562290c5642cde0b2f534b3
Komala MN <komala2001mn@gmail.com>
2026-02-22 22:10:43 +0530

    day3/sum_of_three_numbers.py

---
+++ day3/sum_of_three_numbers.py
@@ -0,0 +1,11 @@
+# Taking input from user
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+# Calculating sum
+total = num1 + num2 + num3
+
+# Printing result
+print("Sum of three numbers is:", total)
